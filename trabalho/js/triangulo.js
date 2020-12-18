ae96af2bd61c92d736a8b68fdd52978870f08220
jorge7s <74425507+jorge7s@users.noreply.github.com>
2020-12-18 10:35:20 -0300

    Update triangulo.js

--- trabalho/js/triangulo.js
+++ trabalho/js/triangulo.js
@@ -8,14 +8,14 @@
         let lc = document.getElementById("lc").value
 
         if (la == lb || la == lc) {
-            document.getElementById ("result").textContent = res1
+            document.getElementById ("resultado2").textContent = res1
         }
 
         else if (la == lb || la == lc || lb == lc) {
-            document.getElementById ("result").textContent = res2
+            document.getElementById ("resultado2").textContent = res2
         }
 
         else {
-            document.getElementById ("result").textContent = res3
+            document.getElementById ("resultado2").textContent = res3
         }
     }
