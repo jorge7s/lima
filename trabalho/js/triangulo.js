d206001f13f4f4ccc933805c34f8c4888977ed65
jorge7s <74425507+jorge7s@users.noreply.github.com>
2020-12-18 10:29:11 -0300

    Update triangulo.js

--- trabalho/js/triangulo.js
+++ trabalho/js/triangulo.js
@@ -1,6 +1,6 @@
-var la = ""
-    var lb = ""
-    var lc = ""
+    var res1 = "Equilatero"
+    var res2 = "Isosceles"
+    var res3 = "Escaleno"
 
     function triangulo() {
         let la = document.getElementById("la").value
@@ -8,14 +8,14 @@ var la = ""
         let lc = document.getElementById("lc").value
 
         if (la == lb || la == lc) {
-            alert("Equilatero")
+            document.getElementById ("result").textContent = res1
         }
 
         else if (la == lb || la == lc || lb == lc) {
-            alert("Isosceles")
+            document.getElementById ("result").textContent = res2
         }
 
         else {
-            alert("Escaleno")
+            document.getElementById ("result").textContent = res3
         }
     }
